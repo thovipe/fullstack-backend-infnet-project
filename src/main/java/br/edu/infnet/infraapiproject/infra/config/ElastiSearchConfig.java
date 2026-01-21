a1658306ace56205d3a4d7b2faf26b2abe9a5335
Thomas <thovipe@gmail.com>
2026-01-21 08:50:20 -0300

    correção na configuração do elastic search e externalizando a configuração da url

--- src/main/java/br/edu/infnet/infraapiproject/infra/config/ElastiSearchConfig.java
+++ src/main/java/br/edu/infnet/infraapiproject/infra/config/ElastiSearchConfig.java
@@ -1,18 +1,23 @@
 package br.edu.infnet.infraapiproject.infra.config;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElastiSearchConfig {
 
+    @Value("${elasticsearch.url}")
+    private String elasticUrl;
+
+
+
     @Bean
     public ElasticsearchClient esClient() {
 
         ElasticsearchClient esClient = ElasticsearchClient.of(b -> b
-                .host("http://localhost:9200")
-
+                .host(elasticUrl)
         );
         return esClient;
     }
